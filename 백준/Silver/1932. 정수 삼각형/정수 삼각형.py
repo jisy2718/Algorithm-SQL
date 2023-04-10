fd82baeb3481f1e8941d7bd3797ced7abc12d163
jisy2718 <97325455+jisy2718@users.noreply.github.com>
2023-04-10 14:08:34 +0900

    [Silver I] Title: 정수 삼각형, Time: 156 ms, Memory: 40628 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,45 +1,31 @@
+# 입력받기
 N = int(input())
-
-arr = [[] for _ in range(N)]
-for i in range(N):
-    arr[i] = list(map(int,input().split()))
-
+arr = [ [] for _ in range(N+1)]
+for i in range(1,N+1):
+    arr[i] = [0] + list(map(int,input().split()))
 #
-# def dfs(row, col, cur_sum):
-#     global max_sum
-#
-#     if row == N-1:
-#         if max_sum < cur_sum:
-#             max_sum = cur_sum
-#
-#     else:
-#         dfs(row + 1, col  , cur_sum + arr[row+1][col])
-#         dfs(row + 1, col+1, cur_sum + arr[row+1][col+1])
-#
-#
-# max_sum = 0
-# dfs(0,0,arr[0][0])
-# print(max_sum)
+# for row in arr:
+#     print(row)
 #
 
+# dp
+dp = [[0]*(i+1) for i in range(0, N+1)]    # 1행에 1개, 2행에 2개 , 3행에 3개,... / dp[r][c]는 r행의 c번째 값 선택시 현재 최대값을 의미
+# for row in dp:
+#     print(row)
+for r in range(1,N+1):
+    for c in range(1,r+1): # r행에는 (r,1) , (r,2) , ... (r, r) 위치가 가능함
+        if r == 1: # 첫 노드 경우
+            dp[r][c] = arr[r][1]
 
+        else:
+            if c != 1 and c !=r : # 중간 노드 경우
+                dp[r][c] = max(dp[r-1][c-1] ,dp[r-1][c] )  + arr[r][c]  # 왼위, 오위
 
-dp = [[0]*i for i in range(1,N+1)]
+            elif c == 1: # 맨 왼편 노드 경우
+                dp[r][c] = dp[r-1][c] +arr[r][c]
 
-r = 0
-while r < N:
-    if r == 0:
-        dp[r][0] = arr[r][0]
-
-    else:
-        for c in range(r+1):  # r번째 row애는 r+1개 col 존재!, 0,1,2,...,r
-            if c == 0 :
-                dp[r][c] = dp[r-1][c] +  arr[r][c]
-            elif c == r :
+            elif c == r: # 맨 오른편 노드 경우
                 dp[r][c] = dp[r-1][c-1] + arr[r][c]
 
-            else: # 0 <c < r
-                dp[r][c] = max(dp[r-1][c-1], dp[r-1][c]) + arr[r][c]
-    r += 1
+print(max(dp[N]))
 
-print(max(dp[N-1]))
