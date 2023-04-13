8a8cc8586be6646320da23c84f061046d851f0a2
jisy2718 <97325455+jisy2718@users.noreply.github.com>
2023-04-13 10:07:08 +0900

    Time: 1215 ms (63.30%), Space: 0B (100.00%) - LeetHub

--- LeetCode-SQL/Easy/1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ LeetCode-SQL/Easy/1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -3,7 +3,8 @@ select product_name, sum(unit) unit from Products p
 left join Orders o
 on p.product_id = o.product_id
 # and o.order_date like '2020-02%'
-and extract(year_month from o.order_date) = 202002
+# and extract(year_month from o.order_date) = 202002
+and left(order_date, 7) = "2020-02"
 
 group by p.product_name
 having sum(unit) >=100
