c990f631c4ac43f30e2f5d771a141db91dc984b1
jisy2718 <97325455+jisy2718@users.noreply.github.com>
2023-04-15 23:31:05 +0900

    Time: 1382 ms (41.25%), Space: 0B (100.00%) - LeetHub

--- LeetCode-SQL/Easy/1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ LeetCode-SQL/Easy/1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -2,4 +2,4 @@
 select * from Users
 where
 # ^ : 뒤의 문자로 문자열 시작, * 앞의 문자열 0개이상 존재
-regexp_like(mail, '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\\.com')
+regexp_like(mail, '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\\.com')  #정규표현식에서 .을 문자 그대로 나타내려면 \\ 2개로 이스케이프 문자 이용 필요 (.은 문자1개를 의미)
