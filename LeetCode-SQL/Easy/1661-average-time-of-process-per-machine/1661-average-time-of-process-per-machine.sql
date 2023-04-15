0094e06502efafef13111275a8e38ea94d5c9d9d
jisy2718 <97325455+jisy2718@users.noreply.github.com>
2023-04-15 23:43:52 +0900

    Time: 551 ms (10.58%), Space: 0B (100.00%) - LeetHub

---
+++ LeetCode-SQL/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+
+select machine_id, round(avg(each_processing_time),3) processing_time 
+
+from
+    (
+    # 각 (machine_id, process_id) 페어별로 걸린 시간 측정
+    select machine_id, process_id, sum(if(activity_type ='start', timestamp*(-1), timestamp))     each_processing_time from Activity
+
+    group by machine_id, process_id
+    ) tmp
+
+group by machine_id
